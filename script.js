aa3b4d5373770670fb1f3324ba78814e68ed6663
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-02-19 07:27:25 -0800

    created a submission

--- script.js
+++ script.js
@@ -10,18 +10,34 @@ const images = [
   { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-function downloadImage(images){
-	loadingDiv.style.display ="block";
-	errorDiv.innerText ="";
-	output.innerHTML="";
-
-	Promise.all(images.map(image => downloadImage(image.url)))
-	.then(results =>{
-		loadingDiv.style.display = "none";
-		results.forEach(img => output.appendChild(img));
-	}).catch(error =>{
-		loadingDiv.style.display = "none";
-		errorDiv.innerText = error.message;
-	});
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image: ${url}`));
+  });
 }
-btn.addEventListener("click",()=>downloadImage(images));
+
+
+function downloadImages(images) {
+  loadingDiv.style.display = "block"; 
+  errorDiv.innerText = ""; 
+  output.innerHTML = ""; 
+
+  
+  Promise.all(images.map(image => downloadImage(image.url)))
+    .then(results => {
+      loadingDiv.style.display = "none"; 
+
+      
+      results.forEach(img => output.appendChild(img));
+    })
+    .catch(error => {
+      loadingDiv.style.display = "none"; 
+      errorDiv.innerText = error.message; 
+    });
+}
+
+
+btn.addEventListener("click", () => downloadImages(images));
