275852c67d2ae870cd6ea68c90eee1a6f58e9554
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-02-19 07:23:53 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
+const loadingDiv = document.getElementById("loading");
+const errorDiv = document.getElementById("error");
 
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(images){
+	loadingDiv.style.display ="block";
+	errorDiv.innerText ="";
+	output.innerHTML="";
+
+	Promise.all(images.map(image => downloadImage(image.url)))
+	.then(results =>{
+		loadingDiv.style.display = "none";
+		results.forEach(img => output.appendChild(img));
+	}).catch(error =>{
+		loadingDiv.style.display = "none";
+		errorDiv.innerText = error.message;
+	});
+}
+btn.addEventListener("click",()=>downloadImage(images));
